e9e7d2e0e809e583476a396cbed8d6b7098d17c5
E-Lop <edmondolopez@gmail.com>
2022-07-22 15:16:11 +0200

    snack 1 completato

--- snack1/index.php
+++ snack1/index.php
@@ -2,8 +2,50 @@
 Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. 
 Ogni array avrà una squadra di casa e una squadra ospite, 
 punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. 
-Stampiamo a schermo tutte le partite con questo schema. -->
+Stampiamo a schermo tutte le partite con questo schema. 
+Olimpia Milano - Cantù | 55-60
+-->
 
 <?php
-    echo 'ciao';
+    $todayBasketMatches = [
+        ['homeTeam' => 'Shohoku',
+        'guestTeam' => 'Toyotama',
+        'homeTeamScore' => 78,
+        'guestTeamScore' => 65],
+        ['homeTeam' => 'Sannoh',
+        'guestTeam' => 'Kainan',
+        'homeTeamScore' => 81,
+        'guestTeamScore' => 104],
+        ['homeTeam' => 'Ryonan',
+        'guestTeam' => 'Shoyo',
+        'homeTeamScore' => 92,
+        'guestTeamScore' => 98],
+    ];
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Le partite di oggi</title>
+</head>
+<body>
+    <h2>Le partite di oggi</h2>
+
+    <ul>
+        <?php for($i = 0; $i < count($todayBasketMatches); $i++) {?>
+            <?php $thisMatch = $todayBasketMatches[$i]; ?>
+            
+            <li>
+                <h3><?php echo $thisMatch[homeTeam]; ?> - <?php echo $thisMatch[guestTeam]; ?> | <?php echo $thisMatch[homeTeamScore]; ?>-<?php echo $thisMatch[guestTeamScore]; ?> </h3>
+                
+            </li>
+            
+            <?php } ?>
+
+    </ul>
+</body>
+</html>
