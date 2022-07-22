8e56d70123a08239c23eaa9a45c81606ea4aa17d
E-Lop <edmondolopez@gmail.com>
2022-07-22 17:13:55 +0200

    integrazione titoli pagine mancanti

--- snack4/index.php
+++ snack4/index.php
@@ -12,7 +12,7 @@ $explodedParagraph = explode('.', $paragraph);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Esplosione del paragrafo</title>
 </head>
 <body>
     <h2>Lorem originale</h2>
--- snack6/index.php
+++ snack6/index.php
@@ -34,7 +34,7 @@ $students = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Media voti degli studenti</title>
 </head>
 <body>
     <h2>Situazione studenti</h2>
